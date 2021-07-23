580cbc8fe9a8775d0256ff0e109ae26ad952b563
Yurk-do <yurk-do@yandex.ru>
2021-07-24 00:55:01 +0300

    task12 completed

--- index.js
+++ index.js
@@ -203,3 +203,33 @@ function getDecade(day) {
 
 // Проверка функции 
 getDecade(11)
+
+// ++++++++++++++ Task 12 ++++++++++++++++
+
+function getTimeOfYear(month) {
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      console.log("winter");
+      break;
+    case 4:
+    case 5:
+    case 6:
+      console.log("spring");
+      break;
+    case 7:
+    case 8:
+    case 9:
+      console.log("summer");
+      break;
+    case 10:
+    case 11:
+    case 12:
+      console.log("autumn");
+      break;
+  }
+}
+
+// Проверка функции 
+getTimeOfYear(11)
